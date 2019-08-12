21319f4f623664de0607d020fc08e13f67438f26
zlluGitHub <18230086651@163.com>
2019-08-12 10:44:08 +0800

    更新

--- pages/goods/index.vue
+++ pages/goods/index.vue
@@ -2,11 +2,11 @@
   <div>
     <h2>商品列表</h2>
     <ul>
-      <li v-for="(item,index) of list" :key="item.title">
-        <a v-if="item.isStatic" :href="'http://localhost:9096/detail/'+item.bid">
+      <li v-for="item of list" :key="item.title">
+        <a v-if="isStatic" :href="'http://localhost:9096/detail/'+item.bid">
           <span>{{item.title}}</span>
         </a>
-        <nuxt-link :to="'/detail/'+item.bid">
+        <nuxt-link v-else :to="'/detail/'+item.bid">
            <span>{{item.title}}</span>
         </nuxt-link>
       </li>
@@ -23,17 +23,13 @@ export default {
   async asyncData(context) {
     console.log(context);
     console.log(process);
-    console.log(process.env.baseUrl)
     return await context.app.$axios
       .get(process.env.baseUrl + "/zllublogAdmin/article/get.article.php")
       .then(res => {
         let data = res.data.list;
-        data.forEach(ele => {
-          ele.isStatic = context.isStatic
-        });
         console.log(data);
-        
         return {
+          isStatic:isStatic,
           list: data
         };
       });
